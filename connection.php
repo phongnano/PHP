b016d87e99335a183e903b0fa7210229a4c9c9f2
phongnano <70794031+phongnano@users.noreply.github.com>
2021-08-24 17:57:11 +0700

    a

--- connection.php
+++ connection.php
@@ -1,4 +1,5 @@
 <?php
-$link = mysqli_connect('ec2-18-214-238-28.compute-1.amazonaws.com', 'fksksdukfwopjs', '4b18a5d24d326aebed06fd62df035a9541852c62f7045283483f8f21685718ef', 'd9dhsg5pgvf1n2', '5432');
+$link = pg_connect('host=ec2-18-214-238-28.compute-1.amazonaws.com dbname=d9dhsg5pgvf1n2 user=fksksdukfwopjs password=4b18a5d24d326aebed06fd62df035a9541852c62f7045283483f8f21685718ef');
 if (!$link)
-    die('Kết nối thất bại ' . mysqli_connect_error());
+    die('Kết nối thất bại');
+pg_close($link);
