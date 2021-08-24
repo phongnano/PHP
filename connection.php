05a3053e5d7bcdc3f994db051f64c46d968c4009
phongnano <70794031+phongnano@users.noreply.github.com>
2021-08-24 16:35:09 +0700

    post

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
 $link = mysqli_connect('ec2-18-214-238-28.compute-1.amazonaws.com
-', 'fksksdukfwopjs', '4b18a5d24d326aebed06fd62df035a9541852c62f7045283483f8f21685718ef', 'd9dhsg5pgvf1n2');
+', 'fksksdukfwopjs', '4b18a5d24d326aebed06fd62df035a9541852c62f7045283483f8f21685718ef', 'd9dhsg5pgvf1n2','5432');
 if (!$link)
     die('Kết nối thất bại ' . mysqli_connect_error());
