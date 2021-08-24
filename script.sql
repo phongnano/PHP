b36cafa94b67ef6f3fdd9add378bc0422a08c3d7
phongnano <70794031+phongnano@users.noreply.github.com>
2021-08-24 16:29:08 +0700

    post

--- script.sql
+++ script.sql
@@ -2,11 +2,11 @@ create table users
 (
     username    varchar(30)                        not null
         primary key,
-    fullname    varchar(50) charset utf8           not null,
+    fullname    varchar(50) 		           not null,
     gender      int                                not null,
     birthday    date                               null,
-    nativeplace varchar(30) charset utf8           null,
-    address     varchar(100) charset utf8          null,
+    nativeplace varchar(30) 		           null,
+    address     varchar(100) 		           null,
     password    varchar(255)                       not null,
     role        int                                not null,
     created_at  datetime default CURRENT_TIMESTAMP null
