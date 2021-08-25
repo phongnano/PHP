0eb2efd1c628c527bfc026c2762feb3a8dbcc4af
phongnano <70794031+phongnano@users.noreply.github.com>
2021-08-25 03:22:31 -0700

    a

--- login.php
+++ login.php
@@ -53,14 +53,18 @@ if (isset($_POST['submit']) && !empty($_POST['submit'])) {
 //        echo '<div class="alert alert-danger" role="alert">NOT OK</div>';
 //        header('location: index.php');
 //    }
-    if (pg_num_rows($data) === 1) {
+    if (pg_num_rows($data) == 1) {
         $row = pg_fetch_assoc($data);
-        if ($row['username'] === $username && $row['password'] === $hashpassword) {
+        if ($row['username'] == $username && $row['password'] == $hashpassword) {
             echo '<div class="alert alert-success" role="alert">OK</div>';
-            header('location: welcome.php');
+            echo $row['username'];
+            echo $row['password'];
+//            header('location: welcome.php');
         } else {
             echo '<div class="alert alert-danger" role="alert">NOT OK</div>';
-            header('location: index.php');
+            echo $row['username'];
+            echo $row['password'];
+//            header('location: index.php');
         }
     }
 }
