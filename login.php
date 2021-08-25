a1a7a06b28577ddc70f39d75ade5e4bceaaf9df5
phongnano <70794031+phongnano@users.noreply.github.com>
2021-08-25 04:23:58 -0700

    a

--- login.php
+++ login.php
@@ -27,10 +27,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 
     if (empty($username_error) && empty($password_error)) {
-        $query = "select * from users where username = '$username' and password = '$password'";
+        $query = "select username, password from users where username = '$username' and password = '$password'";
         $result = pg_query($con, $query);
         $checkLogin = pg_num_rows($result);
-        if ($checkLogin == 1) {
+        if ($checkLogin > 1) {
             echo '<div class="alert alert-danger" role="alert">Đăng nhập thành công</div>';
             header('location: welcome.php');
             exit();
