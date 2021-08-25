49b5686e096de47505aea74d26cb51a9bc5a325a
phongnano <70794031+phongnano@users.noreply.github.com>
2021-08-25 10:17:01 +0700

    a

--- index.php
+++ index.php
@@ -38,9 +38,8 @@
 <?php
 $con = pg_connect('host=ec2-18-214-238-28.compute-1.amazonaws.com dbname=d9dhsg5pgvf1n2 user=fksksdukfwopjs password=4b18a5d24d326aebed06fd62df035a9541852c62f7045283483f8f21685718ef');
 if (isset($_POST['submit']) && !empty($_POST['submit'])) {
-
     $hashpassword = password_hash($_POST['pwd'], PASSWORD_DEFAULT);
-    $sql = "select *from users where username = '" . $_POST['usr'] . "' and password ='" . $hashpassword . "'";
+    $sql = "select *from public.users where username = '" . $_POST['usr'] . "' and password ='" . $hashpassword . "'";
     $data = pg_query($con, $sql);
     $login_check = pg_num_rows($data);
     if ($login_check > 0) {
