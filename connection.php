d8b1a9c0fb9a0f6c3ea83c2bcf9e5b22427df2a8
phongnano <70794031+phongnano@users.noreply.github.com>
2021-08-27 19:01:31 +0700

    a

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$con = pg_connect('host=localhost dbname=postgres user=postgres password=phongnano');
+$con = pg_connect('host=ec2-18-214-238-28.compute-1.amazonaws.com dbname=d9dhsg5pgvf1n2 user=fksksdukfwopjs password=4b18a5d24d326aebed06fd62df035a9541852c62f7045283483f8f21685718ef');
 if (!$con)
     die('Kết nối thất bại');
 
